2fb3d16cc169eb715843b6c6bfd3a4f14dcc8ffa
PEMapModder <pemapmodder1970@gmail.com>
2015-12-30 14:39:02 +0800

    Merged the two repos

---
+++ Pod/README.md
@@ -0,0 +1,2 @@
+# Amoeba-Pods
+Pseudopodia of Amoeba - PocketMine plugin engine for branches of Amoeba-Server
---
+++ Pod/src/Amoeba/Pods/PodsPlugin.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Amoeba-Pods
+ *
+ * Copyright (C) 2015 PEMapModder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PEMapModder
+ */
+
+namespace Amoeba\Pods;
+
+use pocketmine\plugin\PluginBase;
+
+class PodsPlugin extends PluginBase{
+
+}
